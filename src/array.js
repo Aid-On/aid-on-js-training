c4caaed406b6bf8ca1a4e404a505fcd65af4bf9c
atzurody <dunkuandashan9@gmail.com>
2024-12-25 09:16:02 +0900

    途中経過を保存”

---
+++ src/array.js
@@ -0,0 +1,14 @@
+var box = [1,2,3,4,]
+console.log(box)
+box.push(10)
+console.log(box)
+box.pop()
+console.log(box)
+box.shift()
+console.log(box)
+box.unshift(4,5)
+console.log(box)
+
+for(const item of box) {
+  console.log(item)
+}
---
+++ src/loop.js
@@ -0,0 +1,21 @@
+// var count = 0
+// while(count < 10) {
+//  count += 1
+//   console.log("採掘中")
+// }
+
+// for(var x = 0; x < 100; x++) {
+//   console.log(x)
+// }
+
+// var count = 0
+// for(var x = 0; x <= 10; x++) {
+// count += x
+// }
+//   console.log(count)
+
+var count = 0
+for(var x = 0; x <= 20; x++) {
+  count += x
+}
+console.log(count)
